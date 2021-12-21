7be9d28e52a6d4211e5cc19705eb6bada267fb76
Kevblum7 <kevblum77@gmail.com>
2021-12-20 21:12:29 -0500

    Tweet Input Submit

--- TwitterInput.py
+++ TwitterInput.py
@@ -5,50 +5,62 @@ import socket
 import json
 
 
-# Set up your credentials
-consumer_key    = "Ax79DI6gXdMjOCQyQ61v2Khbj"
-consumer_secret = "DBSmfUL50FlhpVnePa373lOpBFffJjrGInXbSGG4T7Ze2RRQTl"
-access_token    = "1418173405988376579-9g3SF5klnWOgd62nUfTfaqrQdzkJRA"
-access_token_secret   = "q3jO28xbGDlvE6djCNeoSMQbtkosZYT7mBTv0O6wznMpw"
+
+class TweetsStream(tweepy.StreamListener):
+
+  def __init__(self, client_socket ):
+      self.client_socket = client_socket
 
 
-class TweetsListener(tweepy.StreamListener):
-
-  def __init__(self, csocket):
-      self.client_socket = csocket
-
-  def on_data(self, data):
+  def on_data(self, packet):
       try:
-          msg = json.loads( data )
-          print(msg['text'].encode('utf-8'))
-          self.client_socket.send( msg['text'].encode('utf-8'))
+          data = json.loads( packet )
+          print(data['text'].encode('utf-8'))
+          self.client_socket.send( data['text'].encode('utf-8'))
           return True
+          
       except BaseException as e:
-          print("Error on_data: %s" % str(e))
+          print("{ERROR!:  %s" % str(e) + " }")
       return True
 
   def on_error(self, status):
       print(status)
       return True
 
-def sendData(c_socket):
-  auth = OAuthHandler(consumer_key, consumer_secret)
-  auth.set_access_token(access_token, access_token_secret)
+def tweetGetter(client_socket, consumer_key, consumer_secret, access_token, access_token_secret ):
 
-  twitter_stream = Stream(auth, TweetsListener(c_socket))
-  twitter_stream.filter(track=['president'])
+  authentication = OAuthHandler(consumer_key, consumer_secret)
+  
+  authentication.set_access_token(access_token, access_token_secret)
+  
+  incoming_tweets = Stream(authentication, TweetsStream(client_socket))
+  
+  incoming_tweets.filter(track=['president'])
 
 if __name__ == "__main__":
-  s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)         # Create a socket object
-  host = "127.0.0.1"     # Get local machine name
-  port = 5556                 # Reserve a port for your service.
-  s.bind((host, port))        # Bind to the port
+    
+  # authentication keys
+  
+  consumer_key = ""
+  consumer_secret = ""
+  access_token = ""
+  access_token_secret = ""
+  
+  port_number = 5556
+  local_host = "127.0.0.1"
 
-  print("Listening on port: %s" % str(port))
+  sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  
+  # connecting local host and port for listening
+  
+  sock.bind((local_host, port_number))
 
-  s.listen(5)                 # Now wait for client connection.
-  c, addr = s.accept()        # Establish connection with client.
+  print("Stream Listener Port: %s" % str(port_number))
 
-  print("Received request from: " + str(addr))
+  sock.listen(5)
+  
+  client, addr = sock.accept()
 
-  sendData(c)
+  print("Connecting to address: " + str(addr))
+
+  tweetGetter(client, consumer_key, consumer_secret, access_token, access_token_secret)
